659cb006ece3f718006d4409c980a85e7789f277
dementive <87823030+dementive@users.noreply.github.com>
2025-08-17 02:10:35 -0500

    Simplify syntax

--- README.md
+++ README.md
@@ -4,56 +4,22 @@ Preprocessor for godot 4.5 gdscript. Adds some nice features that aren't possibl
 
 ## Features
 
-- Use `fn` instead of `func`
-
-- Improved static typing syntax that looks more like C typing for declaring variables
-	For example instead of doing `var test_var: int = 999` you can now do `int test_var = 999`.
-	Or `int test_var` would be `var test_var: int`.
-	This will work for all types, even user defined ones and container types like Array[String].
-	Makes it easier to translate code from gdscript to C#/C++.
-
-- Same static typing rules for typing loop variables.
-	For example `for int i in range(5)` compiles to `for i: int in range(5)`
-
-- Same static typing rules for typing function parameters.
-	For example `func something(int p1, int p2)` compiles to `func something(p1: int, p2: int)`
-
-- Same static typing rules for setget syntax.
-
-- Easier to write `@export` and  `@onready` notation
-	Example: `export String string_export` gets compiled to `@export var string_export: String`
-			 `onready int ready_int_number = 5` gets compiled to `@onready var ready_int_number: int = 5`
-
-- Adds a keyword: `new` that can be used to write `new Object` instead of `Object.new()`. This works for any object type that .new() gets called on.
-
-- Adds a keyword: `free` that can be used to write `free obj` instead of `obj.free()`. This works for any object type that .free() gets called on.
-
-- Adds a keyword: `qfree` that can be used to write `qfree node` instead of `node.queue_free()`. This works for any node type that .queue_free() gets called on.
-
 - `constexpr` keyword for variables that can have their values computed at compile time and are then made const for use in gdscript.
 	The expression after the = operator in a constexpr variable declaration gets evaluated as a godot [Expression](https://docs.godotengine.org/en/stable/classes/class_expression.html).
 	So any code that is a valid Expression can go there and anything in [@GlobalScope](https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html) can be used in a constant expression.
 
-- `consteval` keyword that works the same as constexpr variables except their value will instead be inlined directly everywhere it is used.
+- `consteval` keyword that works the same as constexpr variables except the value will instead be inlined directly everywhere it is used.
 	This way you can precompute values at compile time and also have them not use any memory at run time.
 	consteval variables will not exist as variables in the resulting gdscript, everywhere they are referenced they will be replaced with their value at compile time.
 
-- C style conditional compilation using constant expressions using #if/#else/#endif preprocessor statements
-	The expression after the preprocessor statement can be any constant expressions, [Expression](https://docs.godotengine.org/en/stable/classes/class_expression.html) or constexpr/consteval variables will work.
-
-- `export if` and `onready if` notation that allows conditional compilation of export and onready variables.
-	For example: `export if DEBUG_ENABLED: int export_var_name = 999` will exclude the export variable line from the output if DEBUG_ENABLED is true.
-	The if statement can be any constant expression or constexpr/consteval variable.
-
-- C style preprocessor #include statements that can be used to paste the contents of another file directly where the #include is found.
-
-- C style preprocessor `#define` statements that define simple macros. Multiline macros are also supported. Unlike C macros, there is no way to take in any parameters as I want to prevent issues caused by creating unsearchable tokens with token pasting. Can also use `#undef` to undefine a macro that was previously defined. Be very careful macros, they can make your code very hard to debug if not used carefully.
+- C style conditional compilation with @if/@else/@endif preprocessor statements. The expression after the preprocessor statement can be any constant [Expression](https://docs.godotengine.org/en/stable/classes/class_expression.html) or a constexpr/consteval variable.
 
 - C++ style type aliases with `using CustomAliasName = String` syntax.
 
-- Everything that works in gdscript also works in gdp. So some code can use the new features while normal gdscript can be in the same file with no issues.
+- C style preprocessor `@include` statements that can be used to paste the contents of another file directly where the `@include` is found. The argument must be in double quotes and it must be a valid path to a file that FileAccess::open can open.
 
-To see everything the preprocessor can do check out `test.gdp` and compare it to the gdscript it gets compiled to in `output.gd`.
+- C style preprocessor `@define` statements that define simple macros. Can also use `@undef` to undefine a macro that was previously defined.
+
 
 ## Snippet
 
@@ -61,101 +27,106 @@ Here is a snippet of some cool stuff you can do:
 
 
 ```gdscript
-consteval bool DEBUG_ENABLED = false
+# Alias
+using StringDict = Dictionary[String, String]
+var another_dict: StringDict = {"1": "22", "3": "2"}
 
-export if DEBUG_ENABLED: int debug_number = 999
-onready if not DEBUG_ENABLED: int runtime_number = 9000
+constexpr constexpr_test: float = sqrt(2)
+constexpr constexpr_test2: float = constexpr_test + sqrt(2)
 
-constexpr float constexpr_test = sqrt(2)
-constexpr float constexpr_test2 = constexpr_test + sqrt(2)
+@define TEST_MACRO really_long_variable_name_that_is_very_long
+var TEST_MACRO: int = 0
 
-#define TEST_MACRO really_long_variable_name_that_is_very_long
-int TEST_MACRO = 0
+@define THING var thing1: int = 0
+THING
 
-fn test_func(int x) -> void:
-	Node custom_node = new Node
-	qfree custom_node
+@undef THING
+var THING: int = 0
 
-#define TEST_MULTI_LINE_MACRO \
-	int x = 999 \
-	for int i in range(x): \
-		print(i) \
-	var y = 9999
-
-fn macro_func() -> void:
-	TEST_MULTI_LINE_MACRO
-	print(x - y)
-
-fn macro_func2() -> void:
-	TEST_MULTI_LINE_MACRO
-	print(x + y)
-
-consteval float consteval_test = pow(2, 12)
-fn consteval_usage_test_func(int x) -> int:
-	int value = consteval_test + x
+func constexpr_usage_test_func(x: int) -> int:
+	var value: int = constexpr_test2 + x
 	return value
 
+consteval consteval_test: float = pow(2, 12)
+func consteval_usage_test_func(x: int) -> int:
+	var value: int = consteval_test + x
+	return value
+
+using Union[String,int,float] = Array
+func test_union_alias() -> Union[String,int,float]:
+	return Array()
+
 using Optional[String] = Variant
-fn test_union_alias() -> Optional[String]:
+func test_union_alias2() -> Optional[String]:
 	if 1:
 		return String()
 	else:
 		return null
 
 # Conditional compilation with constant expressions
-
-#if DEBUG_ENABLED
-fn super_secret_debug_function() -> void:
+@if DEBUG_ENABLED
+func super_secret_debug_function() -> void:
 	print("I like cats")
-#else
-fn normal_non_debug_function() -> void:
+@else
+func normal_non_debug_function() -> void:
 	print("I like dogs")
-#endif
+@endif
 
-class Item:
-	int a = 10
+constexpr cond_comp_var: int = 0
+constexpr cond_comp_var_two: int = cond_comp_var + 4
 
-Array[Node] vehicles = [$Car, $Plane]
-Array[Item] items = [new Item]
-Array[Array] array_of_arrays = [[], []]
+consteval power_level: int = sqrt(9001 + sqrt(2) * pow(4, 9)) * 5000
+
+@if power_level > 9000
+func get_favorite_animal() -> String:
+@if DEBUG_ENABLED
+	return "Frogs are my favorite"
+@else
+	return "Rabbits are cool"
+@endif
+@endif
+
+constexpr x: String = "Hello" + " World " + "!"
+constexpr y: Vector3 = Vector3(5, 5, 5)
+constexpr z: int = 999
+consteval TEST: Vector2 = Vector2(0, 0)
+
+@if TEST == Vector2(1, 0)
+func test_test_test():
+	print("hello")
+@endif
 ```
 
 After running through the preprocessor the above code will get transformed into the following gdscript code:
 
 ```gdscript
-@onready var runtime_number: int = 9000
+func global_function_test() -> void:
+	print("Hello World!") # Hi
+
+# Alias
+var another_dict: Dictionary[String, String] = {"1": "22", "3": "2"}
 
 const constexpr_test: float = 1.4142135623730951
 const constexpr_test2: float = 2.8284271247461903
 
 var really_long_variable_name_that_is_very_long: int = 0
 
-func test_func(x: int) -> void:
-	var custom_node: Node = Node.new()
-	custom_node.queue_free()
+var thing1: int = 0
 
+var THING: int = 0
 
-func macro_func() -> void:
-	var x: int = 999 
-	for i: int in range(x): 
-		print(i) 
-	var y = 9999
-
-	print(x - y)
-
-func macro_func2() -> void:
-	var x: int = 999 
-	for i: int in range(x): 
-		print(i) 
-	var y = 9999
-
-	print(x + y)
+func constexpr_usage_test_func(x: int) -> int:
+	var value: int = constexpr_test2 + x
+	return value
 
 func consteval_usage_test_func(x: int) -> int:
 	var value: int = 4096.0 + x
 	return value
 
-func test_union_alias() -> Variant:
+func test_union_alias() -> Array:
+	return Array()
+
+func test_union_alias2() -> Variant:
 	if 1:
 		return String()
 	else:
@@ -165,22 +136,23 @@ func test_union_alias() -> Variant:
 func normal_non_debug_function() -> void:
 	print("I like dogs")
 
-class Item:
-	var a: int = 10
+const cond_comp_var: int = 0
+const cond_comp_var_two: int = 4
 
-var vehicles: Array[Node] = [$Car, $Plane]
-var items: Array[Item] = [Item.new()]
-var array_of_arrays: Array[Array] = [[], []]
 
+func get_favorite_animal() -> String:
+	return "Rabbits are cool"
+
+const x: String = "Hello World !"
+const y: Vector3 = Vector3(5, 5, 5)
+const z: int = 999
 ```
 
 ## Limitations
 
 Still mostly experimental, there are likely some edge cases that can result in broken output.
-The compiler also doesn't report hardly any errors, it mostly just passes everything straight to gdscript.
+The preprocessor also doesn't report hardly any errors, it mostly just passes everything straight to gdscript.
 Please make an issue if you find any bugs.
-
-The code is also definitely not optimal. It's also not really a compiler as it doesn't know hardly anything about the code and doesn't properly parse it. I was mostly just trying to experiment and see if I could get different language features working. It still compiles the tests in 50ms on my computer though so haven't had a need to take time to improve the performance yet.
 
 Debuggablity also suffers a lot when compiling to gdscript. The line numbers do not line up perfectly in the compiled script due to things like conditional compilation. So when you get errors from the gdscript vm at runtime the line number they report might not be the same as the line number was before running the preprocessor. Usually the error gives enough information to figure out the bug but it might make it marginally harder to figure out where errors come from.
 
@@ -191,5 +163,5 @@ To run the tests run godot with `godot --headless --script ./gdp_compiler.gd --q
 Right now there isn't a proper API, only a `compile` function that takes a input file and an output file as parameters.
 I think the ideal way to do it in the future would be to make an EditorPlugin and have it compile automatically on save in the code editor so you can get instant feedback from gdscript's parser and LSP.
 
-The syntax highlighting will be pretty bad in most editors if you use the default gdscript syntax, especially for the preprocessor statements because they are comments in gdscript.
+The syntax highlighting will be pretty bad in most editors if you use the default gdscript syntax.
 If you use Sublime Text you can install [SublimeGodot](https://github.com/dementive/SublimeGodot/tree/a18b34b0e8713899161c31d5522c5fc7603bd6d3/gdp) and use the `gdp` syntax, then if your files have the `gdp` extension you'll get correct highlighting for everything new.
--- gdp_compiler.gd
+++ gdp_compiler.gd
@@ -2,29 +2,11 @@ extends MainLoop
 
 var constexpr_var_pattern: RegEx
 var consteval_var_pattern: RegEx
-
-var memnew_pattern: RegEx
-var memfree_pattern: RegEx
-var memqueue_free_pattern: RegEx
-
-var static_type_func_no_equals_pattern: RegEx
-var annotation_pattern: RegEx
-var annotation_if_pattern: RegEx
-var static_type_pattern: RegEx
-
-var type_no_equals_pattern: RegEx
-var typed_for_pattern: RegEx
 var type_alias_pattern: RegEx
 
 var is_compilation_enabled: bool = true
-var macro_line_continuation: bool = false
-var macro_line_continuation_first: bool = false
-
-var current_macro_value: String = ""
-var current_macro_key: String = ""
 
 var is_compilation_enabled_stack: Array[bool] = []
-
 var macro_dict: Dictionary[String, Variant] = {}
 var constexpr_variables: Dictionary[String, Variant] = {}
 var consteval_variables: Dictionary[String, Variant] = {}
@@ -74,11 +56,8 @@ func compile_regex() -> void:
 	var type_pattern: String = r"([A-Za-z_0-9]\w*(?:\[[^\[\]]+\])?)"
 	var simple_keyword_pattern_fmt: String = r"%s\s+%s"
 
-	var var_declaration_pattern_fmt: String = r"%s\s+%s\s+=(.*)"
-	var var_declaration_pattern: String = var_declaration_pattern_fmt % [type_pattern, var_pattern]
-
-	var full_var_declaration_pattern_fmt: String = r"%s\s+%s\s+=(.*):?"
-	var full_var_declaration_pattern: String = full_var_declaration_pattern_fmt % [type_pattern, var_pattern]
+	var var_declaration_pattern_fmt: String = r"%s:\s+%s\s+=(.*)"
+	var var_declaration_pattern: String = var_declaration_pattern_fmt % [var_pattern, type_pattern]
 
 	var constant_expression_fmt = r"%s\s+%s"
 	constexpr_var_pattern = RegEx.new()
@@ -86,33 +65,6 @@ func compile_regex() -> void:
 
 	consteval_var_pattern = RegEx.new()
 	consteval_var_pattern.compile(constant_expression_fmt % ["consteval", var_declaration_pattern])
-
-	memnew_pattern = RegEx.new()	
-	memnew_pattern.compile(simple_keyword_pattern_fmt % ["new", var_pattern])
-
-	memfree_pattern = RegEx.new()		
-	memfree_pattern.compile(simple_keyword_pattern_fmt % ["free", var_pattern])
-
-	memqueue_free_pattern = RegEx.new()		
-	memqueue_free_pattern.compile(simple_keyword_pattern_fmt % ["qfree", var_pattern])
-
-	static_type_func_no_equals_pattern = RegEx.new()
-	static_type_func_no_equals_pattern.compile(simple_keyword_pattern_fmt % [type_pattern, var_pattern])
-
-	annotation_pattern = RegEx.new()
-	annotation_pattern.compile(r"^(export|onready)\s+{type_pattern}\s+{var_pattern}(.*)".format({"type_pattern": type_pattern, "var_pattern": var_pattern}))
-
-	annotation_if_pattern = RegEx.new()
-	annotation_if_pattern.compile(r"^(export|onready)\s+if\s+(.*):\s+{type_pattern}\s+{var_pattern}(.*)".format({"type_pattern": type_pattern, "var_pattern": var_pattern}))
-
-	static_type_pattern = RegEx.new()
-	static_type_pattern.compile(full_var_declaration_pattern)
-
-	type_no_equals_pattern = RegEx.new()
-	type_no_equals_pattern.compile(r"{type_pattern}\s+{var_pattern}(:?)\n".format({"type_pattern": type_pattern, "var_pattern": var_pattern}))
-
-	typed_for_pattern = RegEx.new()
-	typed_for_pattern.compile(r"for\s+{type_pattern}\s+{var_pattern}".format({"type_pattern": type_pattern, "var_pattern": var_pattern}))
 
 	type_alias_pattern = RegEx.new()
 	type_alias_pattern.compile(r"using\s+{type_pattern}\s+=\s+{type_pattern}".format({"type_pattern": type_pattern}))
@@ -156,39 +108,34 @@ func get_indentation_level(line: String) -> int:
 	return level
 
 func process_line(line: String, file: FileAccess) -> void:
-	if macro_line_continuation:
-		if macro_line_continuation_first:
-			macro_line_continuation_first = false
-			line = line.lstrip("\t")
-		if not line.is_empty():
-			current_macro_value += line + "\n"
-		if not line.ends_with("\\"):
-			macro_line_continuation = false
-			macro_dict[current_macro_key] = current_macro_value.replace("\\", "")
-			current_macro_value = ""
+	# Store lines with only comments
+	if line.begins_with("#"):
+		file.store_line(line)
 		return
 
-	if line.begins_with("#define"):
+	# Macro definition
+	if line.begins_with("@define"):
 		var macro_tokens: PackedStringArray = line.split(" ")
+		var current_macro_value: String = ""
+
 		macro_tokens.remove_at(0)
-		current_macro_key = macro_tokens[0]
+		var current_macro_key: String = macro_tokens[0]
+
 		for i in macro_tokens:
 			if i != current_macro_key:
 				current_macro_value = current_macro_value + " " + i
 
-		if current_macro_value.ends_with("\\"):
-			macro_line_continuation = true
-			macro_line_continuation_first = true
-		elif not current_macro_value.is_empty():
-			macro_dict[current_macro_key] = current_macro_value
+		if not current_macro_value.is_empty():
+			macro_dict[current_macro_key] = current_macro_value.strip_edges()
 
-		current_macro_value = ""
 		return
 
-	if line.begins_with("#undef"):
+	# Macro removal
+	if line.begins_with("@undef"):
 		var macro_tokens: PackedStringArray = line.split(" ")
 		macro_tokens.remove_at(0)
-		current_macro_key = macro_tokens[0]
+
+		var current_macro_key: String = macro_tokens[0]
 		macro_dict.erase(current_macro_key)
 		return
 
@@ -201,22 +148,23 @@ func process_line(line: String, file: FileAccess) -> void:
 	for variable in consteval_variables:
 		line = line.replacen(variable, var_to_str(consteval_variables[variable]))
 
-	if line.begins_with("#if"):
-		var expression: String = line.replace("#if", "")
+	# Conditional compilation
+	if line.begins_with("@if"):
+		var expression: String = line.replace("@if", "")
 		var expression_result = exec_and_return(expression.strip_edges())
 		if (expression_result is bool or expression_result is int):
 			is_compilation_enabled = expression_result
 			is_compilation_enabled_stack.append(is_compilation_enabled)
 		else:
-			print("Wrong type for #if preprocessor statement")
+			print("Wrong type for @if preprocessor statement. Must be a bool or int.")
 
 		return
 
-	if line.begins_with("#else"):
+	if line.begins_with("@else"):
 		is_compilation_enabled = not is_compilation_enabled
 		return
 
-	if line.begins_with("#endif"):
+	if line.begins_with("@endif"):
 		if is_compilation_enabled_stack.size() > 0:
 			is_compilation_enabled = is_compilation_enabled_stack.pop_back()
 		else:
@@ -227,6 +175,7 @@ func process_line(line: String, file: FileAccess) -> void:
 
 		return
 
+	# Type aliases
 	if line.begins_with("using"):
 		var alias_var_match: RegExMatch = type_alias_pattern.search(line)
 		if alias_var_match:
@@ -238,14 +187,11 @@ func process_line(line: String, file: FileAccess) -> void:
 
 		return
 
-	if line.begins_with("#"):
-		file.store_line(line)
-		return
-
-
 	var iter_idx: int = 0
 	var alias_token: String = ""
+
 	for token in line_tokens:
+		# Apply type aliases
 		for alias in type_aliases:
 			if token.ends_with(",") or token.ends_with("]"):
 				alias_token += token
@@ -260,14 +206,18 @@ func process_line(line: String, file: FileAccess) -> void:
 				full_line_str = full_line_str.replace(alias_token, type_convert(type_aliases[alias_token], TYPE_STRING))
 				line_tokens = whitespace_split(full_line_str)
 
-
+		# Apply macros
 		for macro in macro_dict:
-			if token == macro:
-				line_tokens[iter_idx] = type_convert(macro_dict[macro], TYPE_STRING)
+			if token == macro or (token.ends_with(":") and token.left(-1) == macro):
+				if token.ends_with(":"):
+					line_tokens[iter_idx] = type_convert(macro_dict[macro], TYPE_STRING) + ":"
+				else:
+					line_tokens[iter_idx] = type_convert(macro_dict[macro], TYPE_STRING)
 		
 		iter_idx += 1
 
 
+	# Split comments that are on the end of a line
 	var line_split: PackedStringArray = line.split("#")
 	var comment_string: String = ""
 	if line_split.size() > 1:
@@ -278,17 +228,10 @@ func process_line(line: String, file: FileAccess) -> void:
 		return
 
 	var first_token: String = line_tokens[0]
-	var last_token: String = line_tokens[line_tokens.size() - 1]
 
+	# Ignore line if an ifdef has disabled compilation of this line 
 	if not is_compilation_enabled:
 		return
-
-	if first_token == "fn":
-		line_tokens[0] = "func"
-		first_token = "func"
-
-	if (first_token == "static" and line_tokens[1] == "fn"):
-		line_tokens[1] = "func"
 
 	# Constexpr variables
 	if first_token == "constexpr":
@@ -297,12 +240,12 @@ func process_line(line: String, file: FileAccess) -> void:
 
 		var constexpr_var_match: RegExMatch = constexpr_var_pattern.search(line_str)
 		if !constexpr_var_match:
-			print("Compile error on line: ", line)
+			print("Compile error while parsing constexpr variable on line: ", line)
 			print_stack()
 			return
 
 		var groups: PackedStringArray = constexpr_var_match.strings
-		var var_name: String = groups[2]
+		var var_name: String = groups[1]
 		var expression: String = groups[3].strip_edges()
 		var expression_result: Variant = exec_and_return(expression)
 		constexpr_variables[var_name] = expression_result
@@ -330,12 +273,12 @@ func process_line(line: String, file: FileAccess) -> void:
 		var line_str: String = " ".join(line_tokens)
 		var consteval_var_match: RegExMatch = consteval_var_pattern.search(line_str)
 		if !consteval_var_match:
-			print("Compile error on line: ", line)
+			print("Compile error while parsing consteval variable on line: ", line)
 			print_stack()
 			return
 
 		var groups: PackedStringArray = consteval_var_match.strings
-		var var_name: String = groups[2]
+		var var_name: String = groups[1]
 		var expression: String = groups[3].strip_edges()
 		var expression_result = exec_and_return(expression)
 		consteval_variables[var_name] = expression_result
@@ -343,77 +286,6 @@ func process_line(line: String, file: FileAccess) -> void:
 		return
 
 	var line_str: String = " ".join(line_tokens)
-
-	var annotation_if_match: RegExMatch = annotation_if_pattern.search(line_str)
-	if annotation_if_match:
-		var annotation_if_match_groups: PackedStringArray = annotation_if_match.strings
-		var expression: String = annotation_if_match_groups[2]
-		var expression_tokens: PackedStringArray = replace_consteval_vars(expression.split(" "))
-		expression = " ".join(expression_tokens)
-		var expression_result = exec_and_return(expression)
-		if (expression_result and expression_result is bool or expression_result is int):
-			line_str = annotation_if_pattern.sub(line_str, r"@$1 $3 $4 $5").replace(":", "")
-			line_tokens = whitespace_split(line_str)
-		else:
-			return
-
-	line_str = " ".join(line_tokens)
-
-	# Typed function params
-	if line_str.begins_with("func") or line_str.begins_with("static func") or line_str.begins_with("@abstract func"):
-		var no_equals_match: Array[RegExMatch] = static_type_func_no_equals_pattern.search_all(line_str)
-		if no_equals_match.size() > 1:
-			for match: RegExMatch in no_equals_match:
-				var groups: PackedStringArray = match.strings
-				if not groups[1].begins_with("func") and not groups[1].begins_with("static") and not groups[1].begins_with("abstract"):
-					line_str = line_str.replace("%s %s" % [groups[1], groups[2]], "%s: %s" % [groups[2], groups[1]])
-
-	var pattern_match: RegExMatch = memnew_pattern.search(line_str)
-	if pattern_match:
-		line_str = memnew_pattern.sub(line_str, r"$1.new()")  # Allow using "new Object" syntax in place of "Object.new()"
-
-	pattern_match = memqueue_free_pattern.search(line_str)
-	if pattern_match:
-		line_str = memqueue_free_pattern.sub(line_str, r"$1.queue_free()")
-
-	pattern_match = memfree_pattern.search(line_str)
-	if pattern_match:
-		line_str = memfree_pattern.sub(line_str, r"$1.free()")
-
-	line_tokens = whitespace_split(line_str)
-	line_str = " ".join(line_tokens)
-	first_token = line_tokens[0]
-	last_token = line_tokens[line_tokens.size() - 1]
-
-	# onready and export annotations
-	var annotation_match: RegExMatch = annotation_pattern.search(line_str)
-	if annotation_match:
-		line_str = annotation_pattern.sub(line_str, r"@$1 var $3: $2$4")
-
-	# Variable declarations
-	var static_type_var_declaration_match: RegExMatch = static_type_pattern.search(line_str)
-	if static_type_var_declaration_match:
-		var static_type_var_groups: PackedStringArray = static_type_var_declaration_match.strings
-		if (static_type_var_declaration_match and not static_type_var_groups[1] in ["var", "const"]
-			and not (first_token == "func" or first_token == "static func")
-		):
-			line_str = static_type_pattern.sub(line_str, r"var $2: $1 =$3")
-
-	# Variable definitions
-	var no_eq_line_str: String = line_str + "\n"
-	var no_equals_match: RegExMatch = type_no_equals_pattern.search(no_eq_line_str)
-	if no_equals_match and not (first_token == "@abstract" or first_token == "class" or first_token == "for" or first_token == "elif" or first_token == "if" or first_token == "while" or first_token == "match" or first_token == "return" or first_token == "extends" or first_token == "class_name" or first_token == "func"):
-		line_str = type_no_equals_pattern.sub(no_eq_line_str, r"var $2: $1$3")
-
-	if first_token == "const":
-		line_str = line_str.replace("const var", "const")
-
-	# Typed for loop
-	var typed_for_match: RegExMatch = typed_for_pattern.search(line_str)
-	if typed_for_match:
-		var typed_for_match_groups: PackedStringArray = typed_for_match.strings
-		if typed_for_match and typed_for_match_groups[2] != "in":
-			line_str = typed_for_pattern.sub(line_str, r"for $2: $1")
 
 	for level in range(indentation_level):
 		line_str = "\t" + line_str
@@ -430,14 +302,16 @@ func compile(input_file_name: String, output_file_name: String) -> void:
 	while not file.eof_reached():
 		var line: String = file.get_line()
 
-		if line.strip_edges().begins_with("#include"):
-			var include_path: PackedStringArray = line.split("#include")
+		if line.strip_edges().begins_with("@include"):
+			var include_path: PackedStringArray = line.split("@include")
 			var include_path_str: String = include_path[1].strip_edges().replace('"', "")
 
 			var include_file := FileAccess.open(include_path_str, FileAccess.READ)
 			line = include_file.get_as_text()
 			var lines: PackedStringArray = line.split("\n")
 			for included_line in lines:
+				if included_line.is_empty():
+					continue
 				process_line(included_line, out_file)
 
 		process_line(line, out_file)
